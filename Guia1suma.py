f13bc5e09fe57a726243442520d75616e7032094
Dinora965 <roxanapadilla768@gmail.com>
2024-12-10 00:44:50 -0600

    Add files via upload

---
+++ Guia1suma.py
@@ -0,0 +1,6 @@
+def calcular_suma(arreglo):
+    suma = sum(arreglo)
+    return suma
+numeros = [1,2,3,4,5,6,7,8,9,10]
+suma = calcular_suma(numeros)
+print (f"La suma de los elementos del arreglo es:{suma}")
